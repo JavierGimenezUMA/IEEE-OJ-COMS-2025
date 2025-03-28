eb5855d223f7e83301a254e63a8bfec15cc71edd
IC\javierg <jgimenezdlc@uma.es>
2025-03-28 13:33:26 +0100

    Changes in Fig2 scripts and addition of new scripts
    'close all' clause command has been removed from the scripts that generate the results for Fig. 2
    New scripts that launch the scripts corresponding to the results of the same curve in Fig. 2 have been created and included in the repository

---
+++ scripts/Fig2/Launch_Fig2_5G_NR_Ma_0_Md_MT_curves.m
@@ -0,0 +1,22 @@
+clear, close all
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% %                 LAUNCH ALL THE RESULTS THAT BELONG TO THE SAME CURVE IN FIG. 2                   % %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %%
+
+disp('%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%')
+disp('% %                   Displaying results for the 5G NR CURVE: M_a = 0, M_d = M_T                     % %')
+disp('%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% %%')
+
+Reproduce_Fig2_5G_NR_Ma_0_Md_0
+
+Reproduce_Fig2_5G_NR_Ma_0_Md_1
+
+Reproduce_Fig2_5G_NR_Ma_0_Md_2
+
+Reproduce_Fig2_5G_NR_Ma_0_Md_3
+
+Reproduce_Fig2_5G_NR_Ma_0_Md_4
+
+Reproduce_Fig2_5G_NR_Ma_0_Md_5
+
+Reproduce_Fig2_5G_NR_Ma_0_Md_6
